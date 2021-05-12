1a75eaabf65db154788b6589ac8275d0fa6fd4d3
ianrackson <nasirus@protonmail.com>
2021-05-12 01:52:02 -0400

    triple = bug fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import 'App.scss';
 
 function App() {
-    const apiURL = (process.env.NODE_ENV = 'production'
-        ? 'https://irackson-cheese-api.herokuapp.com/'
-        : 'http://localhost:4001/');
+    const apiURL =
+        process.env.NODE_ENV === 'production'
+            ? 'https://irackson-cheese-api.herokuapp.com/'
+            : 'http://localhost:4001/';
 
     return <div className="App">hello world</div>;
 }
